6345adc8608694b39079f32a6e44a05591a5156a
Katherine Kelly <ktkelly11@gmail.com>
2024-05-04 21:22:10 -0400

    added second part of part 1

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ try {
   console.log(counter);
 }
 // got 13956
+
+// This function will take a number and incriment counter to that number
+function incrementTo(num) {
+  if (num <= 0) return;
+  // if (num <=0 || num <= counter) return;
+
+  if (num === counter) return;
+
+  counter += 1;
+  incrementTo(num);
+}
+
+try {
+  incrementTo(10);
+  console.log(counter);
+} catch (err) {
+  console.log(err);
+}
